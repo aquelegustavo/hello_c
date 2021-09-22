2a1677fe2de014dc7babd3dff24c9455ff7753ca
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2021-09-21 23:23:02 -0300

    Add debug mode to 'jogo-do-labirinto'
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

--- examples/jogo-do-labirinto/main.c
+++ examples/jogo-do-labirinto/main.c
@@ -1,40 +1,40 @@
+typedef int bool;
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
-#define NORTE 0
-#define SUL 2
-#define LESTE 1
-#define OESTE 3
-
-#define X 0
-#define Y 1
+#include "definitions.h"
 
 int getRandom(int min, int max);
 int *setMapValue(int columnsNumber, int rowsNumber, int lastBoard[]);
-
+void drawMap(int rowsNumber, int columnsNumber, int map[rowsNumber][columnsNumber], int playerCurrentPosition[]);
+bool checkMove(int rowsNumber, int columnsNumber, int playerCurrentPosition[2], int map[playerCurrentPosition[X]][playerCurrentPosition[Y]]);
 int main()
 {
+    srand(time(0));
 
-    int hiNumberSize = 30,
+    int hiNumberSize = 10,
         lowNumberSize = 5;
 
     int columnsNumber = getRandom(lowNumberSize, hiNumberSize),
         rowsNumber = getRandom(lowNumberSize, hiNumberSize);
 
-    int map[columnsNumber][rowsNumber];
-
-    for (size_t column = 0; column < columnsNumber; column++)
+    if (DEBUG_MODE)
     {
-        for (size_t row = 0; row < rowsNumber; row++)
+        printf("Número de linhas: %i \nNúmero de colunas: %i\n\n\n", rowsNumber, columnsNumber);
+    }
+
+    int map[rowsNumber][columnsNumber];
+    for (size_t row = 0; row < rowsNumber; row++)
+    {
+        for (size_t column = 0; column < columnsNumber; column++)
         {
-            map[column][row] = getRandom(0, 1);
+            map[row][column] = getRandom(0, 1);
         }
     }
 
     int firstColumn = getRandom(0, columnsNumber);
-
-    map[0][firstColumn] = 4; // Primeira casa
 
     int curentRow = 0;
     int *lastBoard;
@@ -46,21 +46,89 @@ int main()
     {
         currentBoard = setMapValue(columnsNumber, rowsNumber, lastBoard);
 
-        printf("X: %i, Y: %i\n", *(currentBoard), *(currentBoard + 1));
+        if (!(currentBoard[X] == rowsNumber))
+        {
 
-        map[*(currentBoard)][*(currentBoard + 1)] = 3;
+            printf("X: %i, Y: %i\n", currentBoard[X], currentBoard[Y]);
+            map[currentBoard[X]][currentBoard[Y]] = DEBUG_MODE ? DEBUG : LIVRE;
+        }
 
-        curentRow = *(currentBoard + 1);
+        curentRow = currentBoard[X];
         lastBoard = currentBoard;
     }
+    map[0][firstColumn] = PLAYER; // Primeira casa
 
-    for (size_t column = 0; column < columnsNumber; column++)
+    int playerCurrentPosition[2] = {0, firstColumn};
+
+    char movimento;
+
+    while ((movimento != 'x') && (movimento != 'X'))
     {
-        for (size_t row = 0; row < rowsNumber; row++)
+        system(LIMPARTELA);
+        drawMap(rowsNumber, columnsNumber, map, playerCurrentPosition);
+
+        map[playerCurrentPosition[X]][playerCurrentPosition[Y]] = LIVRE;
+
+        // Mostrar direções
+        printf("\n\n  (W)\n(A)+(D)\n  (S)\n\n");
+
+        if ((scanf("%c", &movimento)) == 0)
         {
-            printf("%i", map[column][row]);
+            printf("mensagem de erro..\n");
+            getchar();
+            getchar();
+            return ERRO;
         }
-        printf("\n");
+        if ((movimento == 'w') || (movimento == 'W'))
+        {
+            playerCurrentPosition[X] = (playerCurrentPosition[X] - 1);
+
+            if (checkMove(rowsNumber, columnsNumber, playerCurrentPosition, map))
+            {
+                playerCurrentPosition[X] = playerCurrentPosition[X] + 1;
+                printf("\n\nups.. ali não =)\n");
+                getchar();
+                getchar();
+            }
+        }
+        if ((movimento == 's') || (movimento == 'S'))
+        {
+            playerCurrentPosition[X] = playerCurrentPosition[X] + 1;
+
+            if (checkMove(rowsNumber, columnsNumber, playerCurrentPosition, map))
+            {
+                playerCurrentPosition[X] = playerCurrentPosition[X] - 1;
+                printf("\n\nups.. ali não =)\n");
+                getchar();
+                getchar();
+            }
+        }
+        if ((movimento == 'd') || (movimento == 'D'))
+        {
+            playerCurrentPosition[Y] = playerCurrentPosition[Y] + 1;
+
+            if (checkMove(rowsNumber, columnsNumber, playerCurrentPosition, map))
+            {
+                playerCurrentPosition[Y] = playerCurrentPosition[Y] - 1;
+                printf("\n\nups.. ali não =)\n");
+                getchar();
+                getchar();
+            }
+        }
+        if ((movimento == 'a') || (movimento == 'A'))
+        {
+            playerCurrentPosition[Y] = playerCurrentPosition[Y] - 1;
+
+            if (checkMove(rowsNumber, columnsNumber, playerCurrentPosition, map))
+            {
+                playerCurrentPosition[Y] = playerCurrentPosition[Y] + 1;
+                printf("\n\nups.. ali não =)\n");
+                getchar();
+                getchar();
+            }
+        }
+
+        map[playerCurrentPosition[X]][playerCurrentPosition[Y]] = PLAYER;
     }
 
     return 0;
@@ -69,14 +137,11 @@ int main()
 int getRandom(int min, int max)
 {
     int num = (min + rand() % (max + 1 - min));
-    printf("\nNúmero aletório: %i", num);
     return num;
 }
 
 int *setMapValue(int columnsNumber, int rowsNumber, int lastBoard[])
 {
-
-    printf("lastBoard => x: %i, y: %i\n", lastBoard[X], lastBoard[Y]);
 
     int *p;
 
@@ -85,15 +150,15 @@ int *setMapValue(int columnsNumber, int rowsNumber, int lastBoard[])
 
         if (lastBoard[Y] == 0)
         {
-            p = (int[]){LESTE, SUL};
+            p = (int[]){/*length:*/ 2, /*values:*/ LESTE, SUL};
         }
         else if (lastBoard[Y] == columnsNumber)
         {
-            p = (int[]){OESTE, SUL};
+            p = (int[]){/*length:*/ 2, /*values:*/ OESTE, SUL};
         }
         else
         {
-            p = (int[]){LESTE, OESTE, SUL};
+            p = (int[]){/*length:*/ 3, /*values:*/ LESTE, OESTE, SUL};
         }
     }
     else if (lastBoard[X] == rowsNumber)
@@ -102,26 +167,24 @@ int *setMapValue(int columnsNumber, int rowsNumber, int lastBoard[])
     }
     else if (lastBoard[Y] == 0)
     {
-        p = (int[]){LESTE, SUL};
+        p = (int[]){/*length:*/ 2, /*values:*/ LESTE, SUL};
     }
-    else if (lastBoard[Y] == columnsNumber)
+    else if (lastBoard[Y] == (columnsNumber - 1))
     {
-        p = (int[]){OESTE, SUL};
+        p = (int[]){/*length:*/ 2, /*values:*/ OESTE, SUL};
     }
     else
     {
-        p = (int[]){LESTE, OESTE, SUL};
+        p = (int[]){/*length:*/ 3, /*values:*/ LESTE, OESTE, SUL};
     }
-
-    int a[] = {LESTE, OESTE, SUL, 4, 3, 2, 1};
-
-    p = &a;
 
     int *nextBoard;
 
-    //printf("\nTamanho do array: %lu\n", sizeof s / sizeof p[0]);
-
-    switch (1)
+    if (DEBUG_MODE)
+    {
+        printf("ESCOLHA: %i\n", getRandom(1, p[0]));
+    }
+    switch (p[getRandom(1, p[0])])
     {
 
     case LESTE:
@@ -136,4 +199,60 @@ int *setMapValue(int columnsNumber, int rowsNumber, int lastBoard[])
     }
 
     return nextBoard;
+}
+
+void drawMap(int rowsNumber,
+             int columnsNumber,
+             int map[rowsNumber][columnsNumber],
+             int playerCurrentPosition[])
+{
+    for (size_t row = 0; row < rowsNumber; row++)
+    {
+        printf("|");
+        for (size_t column = 0; column < columnsNumber; column++)
+        {
+
+            printf(GREEN " | %zux%zu (%i) " RESET, row, column, map[row][column]);
+
+            switch (map[row][column])
+            {
+            case PLAYER:
+                printf(YELLOW "@" RESET);
+                break;
+            case LIVRE:
+                printf(" ");
+                break;
+            case BLOCO:
+                printf("*");
+                break;
+
+            case DEBUG:
+                printf(RED "#" RESET);
+                break;
+            default:
+                printf(RED "ERRO!" RESET);
+                break;
+            }
+        }
+        printf("|\n");
+    }
+}
+
+bool checkMove(int rowsNumber, int columnsNumber, int playerCurrentPosition[2], int map[playerCurrentPosition[X]][playerCurrentPosition[Y]])
+{
+
+    if (map[playerCurrentPosition[X]][playerCurrentPosition[Y]] == BLOCO)
+    {
+        // Movimento é inválido
+        printf("(%i, %i) É UM BLOCO! \n"
+               "Valor map: % i ",
+               playerCurrentPosition[X],
+               playerCurrentPosition[Y],
+               map[playerCurrentPosition[X]][playerCurrentPosition[Y]]);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
